951e332a16fb1a684501cdd300c5ec89ad65916b
valde12 <s224289@DTU.dk>
2024-03-06 09:08:05 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# Initialize the EV3 Brick
+ev3 = EV3Brick()
+
+# Initialize the motors connected to the wheels
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.C)
+
+# Create a DriveBase object with the initialized motors
+# Adjust the wheel diameter and axle track according to your robot design
+robot = DriveBase(left_motor, right_motor, wheel_diameter=40, axle_track=110)
+robot.settings(straight_speed=200, straight_acceleration=100, turn_rate=100)
+
+gyro_sensor= GyroSensor(Port.S1)
+
+#!/usr/bin/env pybricks-micropython
+# angle = degrees to turn, speed = mm/s
+def turn(angle, speed):
+    gyro_sensor.reset_angle(0)
+    if angle < 0:
+        while gyro_sensor.angle() > angle:
+            left_motor.run(speed=(-1 * speed))
+            right_motor.run(speed=speed)
+            wait(10)
+    elif angle > 0:  
+        while gyro_sensor.angle() < angle:
+            left_motor.run(speed=speed)
+            right_motor.run(speed=(-1 * speed))
+            wait(10)  
+    else:
+        print("Error: no angle chosen")
+
+    left_motor.brake()
+    right_motor.brake()     
+
+# distance = mm, robotSpeed = mm/s
+def drive(distance, robotSpeed):
+    robot.reset() 
+    gyro_sensor.reset_angle(0)
+
+    PROPORTIONAL_GAIN = 1.1
+    if distance < 0: # move backwards
+        while robot.distance() > distance:
+            reverseSpeed = -1 * robotSpeed        
+            angle_correction = -1 * (PROPORTIONAL_GAIN * gyro_sensor.angle())
+            robot.drive(reverseSpeed, angle_correction) 
+            wait(10)
+    elif distance > 0: # move forwards             
+        while robot.distance() < distance:
+            angle_correction = -1 * PROPORTIONAL_GAIN * gyro_sensor.angle()
+            robot.drive(robotSpeed, angle_correction) 
+            wait(10)            
+    robot.stop()
+
+drive(1000, 200)
+
+
+# Stop the motors at the end
+robot.stop()
+
